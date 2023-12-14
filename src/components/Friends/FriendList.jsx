e7199bf48726d418b4482fa4b079ef6f8b158e80
OOlleennaa <123268201+OOlleennaa@users.noreply.github.com>
2023-12-14 14:03:54 +0000

    Update FriendList.jsx

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -4,9 +4,9 @@ import { FriendsStyled, FriendsParam, FriendsStatus, FriendsPhoto,FriendsName } 
 export const FriendList = ({ friends}) => {
     return (
     <FriendsStyled> {friends.map(({ avatar, name, isOnline, id }) => (
-        <FriendsParam key={id}> <FriendsStatus class="status">{isOnline}</FriendsStatus>
-         <FriendsPhoto class="avatar" src="" alt="User avatar" width="48" />{avatar}
-        <FriendsName class="name">{name}</FriendsName> 
+        <FriendsParam key={id}> <FriendsStatus>{isOnline}</FriendsStatus>
+         <FriendsPhoto src={avatar} alt={name} width="48" />{avatar}
+        <FriendsName >{name}</FriendsName> 
          
         </FriendsParam>
       ))}</FriendsStyled>
